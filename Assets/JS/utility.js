66be57bcbd817af4754f821936d1f62423510ab2
bj <jariwala24b@gmail.com>
2022-05-24 22:41:23 +0530

    ne changes

--- Assets/JS/utility.js
+++ Assets/JS/utility.js
@@ -1,15 +1,3 @@
-let elem = document.querySelector(".navbar");
-function scrollAction(){
-    console.log(window.scrollY);
-    elem.classList.toggle("scroll-active",window.scrollY>0);
-    if(window.scrollY > 0){
-        elem.classList.add("scroll-active");
-    }
-    else{
-        elem.classList.remove("scroll-active");
-    }
-}
-
 let hamburger = document.querySelector('.hamburger');
 let menu = document.querySelector('.nav-menu');
 let navbar = document.querySelector('.navbar');
